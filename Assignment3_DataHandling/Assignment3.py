8fe12e05ea8cf14833765a9ea8575e7fd88c3b08
MPauline26 <68587854+MPauline26@users.noreply.github.com>
2024-03-24 11:04:15 +0100

    Adapt data preparation steps

--- Assignment3_DataHandling/Assignment3.py
+++ Assignment3_DataHandling/Assignment3.py
@@ -4,78 +4,117 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import numpy as np
 import math
+from pandas.api.types import is_numeric_dtype
 
+# import data, skip first row "this is a header", delimiter is semicolon
 data = pd.read_csv('/Users/meikeepauline/Desktop/Data/MLB_Ass3_dataa.csv',sep=';', header=None, skiprows=1)
 
+# first row as column names
+data.columns = data.iloc[0]
 
-print(data.head())
-
-data.columns = data.iloc[0] 
+# remove first row (column names) and last row "this is a footer"
 data = data[1:-1]
 
+# print first rows of data
 print(data.head())
 
-#data = data.convert_dtypes()
-
-print("Unique: ", data.alcohol.unique())
-print("teeeeest", data[data.ash == 'missing'])
-print("potato\n", data.loc[~data['alcohol'].astype(str).str.isdigit()])
-
-def comma_to_dot(var):
-    #data[var] = data[var].str.replace(',','.').astype('float')
-    data[var] = data[var].astype('float')
-
-#test = data[data['total_phenols'] == '2,45']
-
+# row 52 has commas as decimal separator -> change to dot
 for i in range(17):
     data.iloc[51:52,i] = data.iloc[51:52,i].str.replace(',','.')
 
+# row 143 has commas and semi colon as delimiter -> concatenate all values to one column
 for x in range(1,17):
     if data.iloc[142:143,x].isna().item() == False:
-        print(x)
         data.loc[142:143,'alcohol'] = data.loc[143:143,'alcohol'] + ',' + data.iloc[142:143,x]
-        print('VERFICKT', data.loc[142:143,'alcohol'])
         data.iloc[142:143,x] = ''
 
+# split data into multiple columns, where it was not correctly done
 data.iloc[50:51,0:17] = data.iloc[50:51,0].str.split(pat=",",expand=True)
 data.iloc[142:143,0:17] = data.iloc[142:143,0].str.split(pat=",",expand=True)
 data.iloc[166:167,4:6] = data.iloc[166:167,4].str.split(pat=" ",expand=True)
 
+# split coulmn "country-age" to two columns and drop
+data[['country','age']] = data['country-age'].str.split(pat="-",expand=True)
+data.drop('country-age', axis=1, inplace=True)
+
+# remove 'years from column age
+data['age'] = data.age.str.replace('years','')
+
+# column ash has value == 'missing' -> replace with "NaN"
 data.ash = data.ash.replace('missing',np.nan)
 
-# alternative: data.loc[142:143,'alcohol'] = data.iloc[142:143,0] + ',' + data.iloc[142:143,1]
+# convert columns to type float, if error -> skipped
+for i in range(18):
+    data.iloc[:,i:i+1] = data.iloc[:,i:i+1].astype('float', errors = 'ignore')
 
-#data.loc[142:143,:] = 
-#test = pd.DataFrame(data.iloc[142:143,1:].shift(periods=5,axis="columns"))
-#data.iloc[142:143,1:17] = test
-#print("FUUUCK", test, "\n", data.iloc[142:144,])
-#data.iloc[50:51,0:17] = data.iloc[50:51,0].str.split(pat=",",expand=True)
-#data.iloc[142:143,1:17].shift(periods=5, axis="columns")
+# automatically convert datatype
+data = data.convert_dtypes()
+print("----------------------",data.dtypes)
 
-comma_to_dot('alcohol')
-comma_to_dot('malic_acid') #replace , with .
-comma_to_dot('ash') #--remove 'missing'
-comma_to_dot('alcalinity_of_ash') #replace , with .
-comma_to_dot('magnesium') #--remove '111 1.7'
-comma_to_dot('total_phenols') #replace , with .
-comma_to_dot('flavanoids') #replace , with .
-comma_to_dot('nonflavanoid_phenols') #replace , with .
-comma_to_dot('proanthocyanins') #replace , with .
-comma_to_dot('color_intensity') #replace , with .
-comma_to_dot('hue') #replace , with .
-comma_to_dot('od280/od315_of_diluted_wines') #replace , with .
-comma_to_dot('proline') #no problems
-comma_to_dot('color') #no problems
-comma_to_dot('target') #no problems
+# determine invalid data entries
+print("------------ Unique values: ------------\n")
+for col in data:
+    print("------------",  str(col), "\n", data[col].unique(), "\n")
 
-data2 = data.convert_dtypes()
-print("----------------------",data2.dtypes)
+# set invalid data entries to null, correct inconsistent data entries for 'season'    
+data.malic_acid = data.malic_acid.replace(-999,np.nan)
+#data.proline = data.proline.replace(0,np.nan)
+data.season = data.season.replace('spring','SPRING')
+data.season = data.season.replace('aut','AUTUMN')
 
-#for i in range(15):
-#    data.iloc[:,i] = data.iloc[:,i].astype('float')
+# find duplicate rows and delete
+print(data[data.duplicated(keep=False)])
+data.drop(labels=[179,180], axis=0, inplace=True)
 
-print("HEEEERE\n", data.iloc[50:53,],"\n", data.iloc[140:144,],"\n", data.iloc[133:140,])
-print(data.head())
-print(data.columns)
-print(data.dtypes)
-print(data.index)
+# rename column od280/od315_of_diluted_wines to diluted_wines
+data.rename(columns = {'od280/od315_of_diluted_wines':'diluted_wines'}, inplace = True)
+
+# print statistics
+print(data.total_phenols.describe())
+
+#def histogram(var):
+for col in data:
+    sns.displot(data[col])
+    # if is_numeric_dtype(var) == True:
+    #     plt.hist(var[~np.isnan(var)])
+    #     plt.title(str(var))
+    # else:
+    #     plt.hist(var)
+    #     plt.title(str(var))
+
+# histogram(data.alcohol)
+# histogram(data.malic_acid)
+# histogram(data.ash)
+# histogram(data.alcalinity_of_ash)
+# histogram(data.magnesium)
+# histogram(data.total_phenols) #outliers
+# histogram(data.flavanoids)
+# histogram(data.nonflavanoid_phenols)
+# histogram(data.proanthocyanins)
+# histogram(data.color_intensity)
+# histogram(data.hue)
+# histogram(data.diluted_wines)
+# histogram(data.proline)
+# histogram(data.season)
+# histogram(data.target)
+# histogram(data.country)
+# histogram(data.age)
+
+plt.show()
+
+data.proline = data.proline.replace(0,np.nan)
+data['total_phenols'] = data['total_phenols'].where(data['total_phenols']<148, other=0)
+
+
+#print(data.head())
+#print(data.columns)
+#print(data.dtypes)
+#print(data.index)
+
+# malic_acid -> -999
+
+#alcohol, ash -> na
+
+# proline -> 0
+
+#season -> vereinheitlichen
