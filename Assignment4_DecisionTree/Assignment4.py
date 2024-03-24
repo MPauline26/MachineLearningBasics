63e5b44368652496c1179af392bd6fbb115385ff
MPauline26 <68587854+MPauline26@users.noreply.github.com>
2024-03-24 11:10:04 +0100

    Refinement of split process

--- Assignment4_DecisionTree/Assignment4.py
+++ Assignment4_DecisionTree/Assignment4.py
@@ -5,93 +5,86 @@ warnings.simplefilter(action='ignore', category=FutureWarning)
 # import packages
 from sklearn.datasets import make_classification
 from sklearn.model_selection import KFold
+from sklearn.model_selection import train_test_split
 from math import sqrt
 import pandas as pd
 import numpy as np
 
+# set constants
 SEED_DATASET = 20220919
 ROWS_NUM = 100
 FEATURES_NUM = 4
 
 # create dataset
 X,y = make_classification(n_samples=ROWS_NUM,n_features=FEATURES_NUM,random_state=SEED_DATASET, class_sep=2)
-
 data_X = np.array(X)
 data_y = np.array(y)
-data = pd.DataFrame(np.append(data_X ,data_y.reshape(-1,1),axis=1), columns=['test1','test2','test3','test4','target'])
+data = pd.DataFrame(np.append(data_X ,data_y.reshape(-1,1),axis=1), columns=['VAR1','VAR2','VAR3','VAR4','target'])
 data.target = data.target.astype('int')
-print(data.head())
 
-def gini_calculation(data_input, var, value,target):
+# splitting data into test and train
+train, test = train_test_split(data, test_size=0.2)
+print(train.shape, test.shape)
 
-    data['temp_var'] = np.where(data[var]<value, 'Bucket1', 'Bucket2')
+def gini_calculation(data_input, var, value, target):
+
+    # create temporary variable to split variable into two buckets
+    data_input['temp_var'] = np.where(data_input[var]<value, 'Bucket1', 'Bucket2')
     
+    # count frequency of target in each bucket
     count = data_input.groupby([pd.Categorical(data_input.target), 'temp_var']).size().fillna(0)
     count = np.array(count.values.tolist())
     count = np.resize(count, (2, 2))
 
+    # count total frequency of each bucket
     total = data_input.value_counts(["temp_var"]).sort_index()
     total = total.to_numpy()
     total = np.resize(total, (1,2))
 
+    # calculate gini of each bucket
     gini_indiv = (1 - np.sum((count/total)**2, axis = 0))
-    weights = np.sum(count, axis=0)/len(data_input.index)
 
+    # calculate weights and total gini
+    weights = np.sum(count, axis=0)/len(data_input.index)
     gini = np.sum(gini_indiv*weights)
 
-    # print("Count:\n", data_input.value_counts(["temp_var", target]).sort_index())
-    # print("Count:\n", count)
-    # print(type(count), count.shape)
-    # print("Total:\n", total)
-    # print("Count/Total:\n", count/total)
-    # print("Weight:\n", weights)
-    # print("Gini individually:",gini_indiv)
-    # print("Gini:",gini)
-    # print(data.head())
-
-    data.drop(['temp_var'], axis=1, inplace=True)
+    # drop temporary variable
+    data_input.drop(['temp_var'], axis=1, inplace=True)
 
     return gini
 
-#gini_calculation(data, 'test2', -0.454876, 'target') # nearly perfect split, seed: 20220919
-gini_calculation(data, 'test2', -0.454876, 'target')
+# create list of possible splits (all unique values of all features)
+gini_list = pd.DataFrame(columns = ['variable','unique_values','gini'])
 
-data_list = pd.DataFrame(columns = ['variable','unique_values','gini'])
-
-for column in data:
+for column in train:
     if column == 'target':
         continue
-    print(column)
+
+    # create temporary DataFrame to append list
     temp = pd.DataFrame()
-    temp['unique_values'] = pd.DataFrame(set(data[column]))
+    temp['unique_values'] = pd.DataFrame(set(train[column]))
     temp['variable'] = column
-    data_list = data_list.append(temp, ignore_index=True)
+    gini_list = gini_list.append(temp, ignore_index=True)
 
-for index, row in data_list.iterrows():
-    data_list.iloc[index, 2] = gini_calculation(data, row['variable'], row['unique_values'], 'target')
+# calculate ginis for all splits
+for index, row in gini_list.iterrows():
+    gini_list.iloc[index, 2] = gini_calculation(train, row['variable'], row['unique_values'], 'target')
 
-data_list = data_list.sort_values(by=['gini']).reset_index(drop=True)
+# sort list by ascending gini
+gini_list = gini_list.sort_values(by=['gini']).reset_index(drop=True)
 
-print(temp.head())
-print(data_list.head())
-print(data_list.tail())
+# determine most occuring class per split
+class1 = train[train[gini_list.iloc[0,0]]<gini_list.iloc[0,1]].target.mode()
+class2 = train[train[gini_list.iloc[0,0]]>=gini_list.iloc[0,1]].target.mode()
 
-#SPLIT_VAR = str(data_list[0,0])
-#SPLIT_NUM = data_list[0,1]
-
-print(data_list.iloc[0,0])
-print(data_list.iloc[0,1])
-
-class1 = data[data[data_list.iloc[0,0]]<data_list.iloc[0,1]].target.mode()
-class2 = data[data[data_list.iloc[0,0]]>=data_list.iloc[0,1]].target.mode()
-
-print(data.head())
-print(class1.head())
-print(class2.head())
+# variable to split: gini_list.iloc[0,0]
+# value to split by: gini_list.iloc[0,1]
+# class in first branch (<  than x): class1
+# class in first branch (>= than x): class2
 
 # validation
 data_test = data.copy()
-data_test['test'] = np.where(data_test[data_list.iloc[0,0]]<data_list.iloc[0,1], class1, class2)
+data_test['test'] = np.where(data_test[gini_list.iloc[0,0]]<gini_list.iloc[0,1], class1, class2)
 print(data_test.head())
 
 data_test['Correctly_Predicted'] = np.where(data_test['target'] == data_test['test'], True, False)
